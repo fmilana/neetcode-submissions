428d5799b8cd262ef0da7f21cdd8fcab32722118
Federico Milana <federicomilana@outlook.com>
2026-09-09 15:10:49 +0100

    Add: python-custom-heap - submission-0

---
+++ Python Coding Interviews/python-custom-heap/submission-0.py
@@ -0,0 +1,19 @@
+import heapq
+from typing import List
+
+
+def get_reverse_sorted(nums: List[int]) -> List[int]:
+    heap = []
+    output = []
+    for num in nums:
+        heapq.heappush(heap, (-num, num))
+    while heap:
+        pair = heapq.heappop(heap)
+        output.append(pair[1])
+    return output
+
+
+# do not modify below this line
+print(get_reverse_sorted([1, 2, 3]))
+print(get_reverse_sorted([5, 6, 4, 2, 7, 3, 1]))
+print(get_reverse_sorted([5, 6, -4, 2, 4, 7, -3, -1]))
